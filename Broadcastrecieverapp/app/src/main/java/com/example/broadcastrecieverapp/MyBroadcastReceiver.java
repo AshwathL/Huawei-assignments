6a7c1cd8b7579b8df499f7e39106d2714c0c535f
Ashwath L <ashwath.aer@gmail.com>
2022-04-01 00:35:49 +0530

    assignment 28.032022

---
+++ Broadcastrecieverapp/app/src/main/java/com/example/broadcastrecieverapp/MyBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package com.example.broadcastrecieverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Log.i("BroadcastReceiver", "airplane mode on");
+        Toast.makeText(context,"airplane mode on ",Toast.LENGTH_LONG).show();
+    }
+}
